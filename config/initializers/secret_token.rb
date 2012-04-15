21bd8b3ee6921b89a1cd4f4d5b5a8e440d015810
Mikhail Bortnyk <vessimir@gmail.com>
2012-04-15 17:36:51 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leprocards::Application.config.secret_token = 'ad7ea50ca5dcdf51ee437b99f0a11f8e63b6b8ab9c2b07bfb8102259c11860659386a830fff0cc3c191200d5836bd1307758a9bca6b6a0f54c4111a02dfbc878'
