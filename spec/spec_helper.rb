5713f0ff76690b48cb184376244038a4e660c0b6
Mikhail Bortnyk <vessimir@gmail.com>
2012-04-21 12:51:56 +0300

    Fixed capybara & selenium, tests working now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/rails'
+require 'capybara/rspec'
 require 'simplecov'
 
 SimpleCov.start
